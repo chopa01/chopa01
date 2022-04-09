04f5e18ad3dd844f82e7bc81b17c061411be1212
chopa01 <xheo@naver.com>
2022-04-09 08:34:11 +0000

    몇건몇건처리

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -8,6 +8,7 @@
  
             <ul class="todo_list">
                 <li class="todo_list_tit"><p>할 일</p></li> 
+				<li><p>{{remaining}} / {{todolist.length}} 건 처리 </p></li>
 				<li class="del_btn" v-for="(item, index) in todolist" :key="index">
                     <p :class="{doneStyle:item.done}"> <input type="checkbox" name="check1" v-model="item.done">{{item.todo}}  </p>
 					 <ul class="todo_list_btn">
@@ -42,6 +43,15 @@ export default {
 	 ]
   }
   },  
+	computed:{
+		remaining() {
+			return this.todolist.filter(function(val){
+				console.log(val);
+				return val.done;
+			}).length; 
+		 
+		}
+	},
 	methods: {
 		addTodo(val) {
 	 		this.todolist.push({done:false, todo:val});
